ad3e1b90375cf8db6c5a0ad77220f40da16e5aec
Indunil1992 <indunil@adroitlogic.com>
2019-07-08 14:20:48 +0530

    10

--- indunilSQStging/lambda1.js
+++ indunilSQStging/lambda1.js
@@ -4,6 +4,9 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
 
+
+    
+
     sqs.receiveMessage({
         QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
         AttributeNames: ['All'],
