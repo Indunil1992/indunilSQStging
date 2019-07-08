d3c0ed9e850d361e5501a31903bf34d601c8b78b
Indunil1992 <indunil@adroitlogic.com>
2019-07-08 15:01:23 +0530

    l

--- indunilSQStging/lambda1.js
+++ indunilSQStging/lambda1.js
@@ -4,6 +4,35 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
 
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                    console.log("Success");
+                 console.log( data );
+                });
+            } else {
+                 console.log("No messages");
+                 console.log( data );
+                // No messages to process
+            }
+        })
+        .catch(err => {
+            // error handling goes here
+            console.log("error");
+            console.log( data );
+        });
+
+        
 
 
 
