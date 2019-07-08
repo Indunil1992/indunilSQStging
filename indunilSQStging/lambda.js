08908496dc9ff1bd87d15be89fce0762b9cbfcfc
Indunil1992 <indunil@adroitlogic.com>
2019-07-08 15:07:44 +0530

    x

--- indunilSQStging/lambda.js
+++ indunilSQStging/lambda.js
@@ -4,15 +4,19 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
 
-    
+
 
     sqs.sendMessage({
         MessageBody: 'testtt',
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
         DelaySeconds: '0',
         MessageAttributes: {
             "x": {
                 "DataType": "String",
+                "StringValue": "a"
+            },
+            "sa": {
+                "DataType": "Number",
                 "StringValue": "1"
             }
         }
