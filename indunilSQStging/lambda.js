fcdee058bc320af7b0dfa1ed8b1cdc6d8de13f8b
Indunil1992 <indunil@adroitlogic.com>
2019-07-08 13:00:26 +0530

    l

--- indunilSQStging/lambda.js
+++ indunilSQStging/lambda.js
@@ -1,5 +1,5 @@
 exports.handler = function(event, context, callback) {
     
     
-    callback(null, {"message": "Successfully executed with nw ww"});
+    callback(null, {"message": "Successfully executed with nw s1"});
 }
